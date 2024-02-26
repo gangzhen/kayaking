50ee6536d8970132eaa6eba09bc72aad71d59600
gangzhen <gangzhen2002@gmail.com>
2024-02-27 00:05:14 +0800

    update global variable

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/assets/css/global.css'
 import '@/assets/icons/index.js'
+import '@/utils/globalVarService.js'
 
 Vue.config.productionTip = false
 
---
+++ frontend/src/utils/globalVarService.js
@@ -0,0 +1,6 @@
+import Vue from "vue";
+
+Vue.prototype.$groupOptions = [
+    {label: '男子组', value: 'male',},
+    {label: '女子组', value: 'female',},
+];
